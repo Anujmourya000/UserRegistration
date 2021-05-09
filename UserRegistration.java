956b583183247a847c1ded2721b1756b221bb56a
Anujmourya000 <Anujmourya000@gmail.com>
2021-05-10 02:08:59 +0530

    Added: phone number is added

--- UserRegistration.java
+++ UserRegistration.java
@@ -30,5 +30,27 @@ public class UserRegistration {
             System.out.println("Its not a valid last name...\n correct format is ( e.g 'Mathew')");
 
         }
+
+        System.out.println("Enter the valid email address: ");
+        String email = sc.nextLine();
+        boolean result3 = Pattern.compile("^[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]{2,}+)*$").matcher(email).matches();
+        if(result3 == true){
+            System.out.println("you have entered a valid email address");
+        }else{
+            System.out.println("Its not a valid email...\n correct format is ( e.g 'xyz21@abc.com')");
+
+        }
+
+        System.out.println("Enter the valid phone number: ");
+        String phone = sc.nextLine();
+        boolean result4 = Pattern.compile("^[0-9]{1,3} [0-9]{10}$").matcher(phone).matches();
+        if(result4 == true){
+            System.out.println("you have entered a valid phone number");
+        }else{
+            System.out.println("Its not a valid phone number...\n correct format is ( e.g '91 1234567890')");
+
+        }
+
+
     }
 }
